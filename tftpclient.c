5dfbe8ed7cdc6561e7287ca847a05d55dfc7c0c0
James Mack <jdmack@ucsd.edu>
2013-05-21 05:49:54 -0700

    client code cleanup

--- tftpclient.c
+++ tftpclient.c
@@ -19,10 +19,6 @@ char test_packet[] = {0x00, 0x04, 0x00, 0x01};
 char op;
 char * filename = NULL;
 
-
-
-
-void dg_cli(int sockfd, struct sockaddr * pserv_addr, int servlen, char op, char * filename);
 void process_cl_args(int argc, char *argv[]);
 
 //================================================================================
@@ -32,30 +28,16 @@ void process_cl_args(int argc, char *argv[]);
 //================================================================================
 void main(int argc, char *argv[])
 {
-    int sockfd;
+    int sockfd, n, i;
     struct sockaddr_in serv_addr, cli_addr;
+    char sendline[MESSAGE_SIZE], recvline[MESSAGE_SIZE + 1];
 
     // Display init message
     printf("Group #06 Client\n");
     printf("Members: James Mack\n");
 
     // Create local socket
-    //sockfd = setup_socket(INADDR_ANY, 0);
-
-    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("can't open datagram socket\n");
-        exit(1);
-    }
-
-    bzero((char *) &cli_addr, sizeof(cli_addr));
-    cli_addr.sin_family      = AF_INET;
-    cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    cli_addr.sin_port        = htons(0);
-
-    if(bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
-        printf("can't bind local address\n");
-        exit(2);
-    }
+    sockfd = setup_socket(INADDR_ANY, 0);
 
     // Outgoing
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -63,34 +45,21 @@ void main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
     serv_addr.sin_port        = htons(SERVER_PORT);
 
-    dg_cli(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr), op, filename);
+    printf("sending message\n");
+
+    sendto(sockfd, test_packet, MESSAGE_SIZE, 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+    n = recvfrom(sockfd, recvline, MESSAGE_SIZE, 0, NULL, NULL);
+
+    if(n < 0) {
+        printf("recvfrom error\n");
+        exit(4);
+    }
+    
+    recvline[n] = 0;
 
     close(sockfd);
     exit(0);
-}
-
-void dg_cli(int sockfd, struct sockaddr * pserv_addr, int servlen, char op, char * filename)
-{
-    int n, i;
-    char sendline[MESSAGE_SIZE], recvline[MESSAGE_SIZE + 1];
-    int op_complete = 0;
-
-   // while(!op_complete) {
-        
-        printf("sending message\n");
-
-        sendto(sockfd, test_packet, MESSAGE_SIZE, 0, pserv_addr, servlen);
-
-        n = recvfrom(sockfd, recvline, MESSAGE_SIZE, 0, NULL, NULL);
-
-        if(n < 0) {
-            printf("recvfrom error\n");
-            exit(4);
-        }
-        
-        recvline[n] = 0;
-        //fputs(recvline, stdout);
-    //}
 }
 
 //================================================================================
